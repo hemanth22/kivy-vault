fdd309d95282a33b3fb0e7fe8626e33ddfc790ae
Hemanth B <hemanth22hemu@gmail.com>
2023-07-02 22:47:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from kivy.properties import ObjectProperty
 from kivy.core.clipboard import Clipboard
 from kivy.lang import Builder
 import json
+from Cryptodome.Cipher import Blowfish
+from Cryptodome.Util.Padding import pad, unpad
+from base64 import b64encode, b64decode
 
 Builder.load_file('hike.kv')
 
@@ -16,15 +19,26 @@ class MyGridLayout(Widget):
 
     oldsalary = ObjectProperty()
 
-    def simpleiint(self):
-        #with open('data.json') as f:
-        #    data = json.load(f)
 
-        jdata = '{"hemanth": "bitra","priyadharshini": "purushotham"}'
+
+
+    def simpleiint(self):
+        jdata = '{"hemanth": "0d4ba01661c2de80","priyadharshini": "20bc3aa6fbf6b2c0e9f622c36f5a20db"}'
         data = json.loads(jdata)
 
+        secret_key = b'bitroid'
+        bf = Blowfish.new(secret_key, Blowfish.MODE_ECB)
+
+        def decrypt_password(encrypted_password: str):
+            cipher_text = bytes.fromhex(encrypted_password)
+            decrypted_text = bf.decrypt(cipher_text)
+            unpadded_text = unpad(decrypted_text, Blowfish.block_size).decode('utf-8')
+            return unpadded_text
+        
+
         oldsalary = self.oldsalary.text
-        result = data.get(oldsalary)
+        #result = data.get(oldsalary)
+        result = decrypt_password(data[oldsalary])
         #newsalary = self.newsalary.text
         #result_percent = round((((int(newsalary)-int(oldsalary)) / int(oldsalary)) * 100),2)
         #print('Your hike {0}%'.format(result_percent))
